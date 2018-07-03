a08d2dac6f19c44763a48dfdf977ed626c23e2ab
Oscar Gracia <oscar.graciae@gmail.com>
2018-07-02 23:45:54 -0500

    Cambio de coordenadas

--- .next/app.js
+++ .next/app.js
@@ -90,7 +90,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "c3bbc4cebb2c4b4fde3d"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "abee4b2eef20b66ab4ba"; // eslint-disable-line no-unused-vars
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
@@ -755,7 +755,7 @@
 /******/ 		if (__webpack_require__.nc) {
 /******/ 			script.setAttribute("nonce", __webpack_require__.nc);
 /******/ 		}
-/******/ 		script.src = __webpack_require__.p + "" + ({"0":"main.js","1":"bundles/pages/_error.js","2":"commons","4":"chunks/components_map_MapZone_49a06d2b3ff73dc2fdb3ce0b400916f7","5":"bundles/pages/menu.js","6":"bundles/pages/login.js","7":"bundles/pages/signup.js","8":"bundles/pages/passwordReset.js"}[chunkId]||chunkId) + "-" + {"0":"3eae8f0b9aaf82e89f2a","1":"dbedf8264eff5ac2eab4","2":"112a70b41bfbcd7c6dde","4":"5e11e95e6d2974c492aa","5":"78cfe7c9ea156096b60b","6":"e5b08de2bad8a08b4022","7":"5ce30a0d02d860c8152c","8":"95510b2b663467d7c063"}[chunkId] + ".js";
+/******/ 		script.src = __webpack_require__.p + "" + ({"0":"main.js","1":"bundles/pages/_error.js","2":"commons","4":"chunks/components_map_MapZone_49a06d2b3ff73dc2fdb3ce0b400916f7","5":"bundles/pages/index.js","7":"bundles/pages/menu.js","8":"bundles/pages/checkout.js","9":"bundles/pages/mapAvailable.js"}[chunkId]||chunkId) + "-" + {"0":"3eae8f0b9aaf82e89f2a","1":"dbedf8264eff5ac2eab4","2":"112a70b41bfbcd7c6dde","4":"e4f5e00dd41d18ce637d","5":"f99fa9420b701c6e3808","7":"ea67c6ad6e7b4f81f45d","8":"68cf625b3bcfdcdfc3b2","9":"b658c939fa14c96a4deb"}[chunkId] + ".js";
 /******/ 		var timeout = setTimeout(onScriptComplete, 120000);
 /******/ 		script.onerror = script.onload = onScriptComplete;
 /******/ 		function onScriptComplete() {
